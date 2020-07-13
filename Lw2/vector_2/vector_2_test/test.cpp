1719f1622a401e3fbb40ef7c2795897e55383053
AlexeyAndrianov <52441522+KrovYR@users.noreply.github.com>
2020-07-13 16:57:16 +0300

    Update test.cpp

--- Lw2/vector_2/vector_2_test/test.cpp
+++ Lw2/vector_2/vector_2_test/test.cpp
@@ -1,22 +1,56 @@
 #define CATCH_CONFIG_MAIN
-#include "../../catch/catch.hpp"
+#include "../../../catch/catch.hpp"
 #include "../vector_2/function.h"
 #include <iostream>
 
-TEST_CASE("Check_vector_with_zero")
+TEST_CASE("Check_function_ReadNumbers")
 {
-	std::vector<double> numbers = { 0, 1, 2, 3, 4 };
-	std::vector<double> output = { 0, 0, 0, 0, 0 };
-	MultiplyByLowerValue(numbers);
-	SortNumbersAscending(numbers);
-	REQUIRE(output == numbers);
+	std::istringstream stringStream("10 2.4 7.9 0 11");
+	std::istream stream(stringStream.rdbuf());
+
+	std::vector<double> nonEmptyVector{ 10, 2.4, 7.9, 0, 11 };
+
+	REQUIRE(ReadNumbers(stream) == nonEmptyVector);
 }
 
-TEST_CASE("Check_vector_sort")
+TEST_CASE("Check_function_GetLowerValue")
 {
-	std::vector<double> numbers = { 6, 3, 2, 1, 4, 5 };
-	std::vector<double> output = { 1, 2, 3, 4, 5, 6 };
-	MultiplyByLowerValue(numbers);
-	SortNumbersAscending(numbers);
-	REQUIRE(output == numbers);
+	std::vector<double> nonEmptyVector{ 10, 2.4, 7.9, 0, 11 };
+
+	double LowerValue = 0;
+
+	REQUIRE(GetLowerValue(notEmptyVector) == LowerValue);
+}
+
+TEST_CASE("Check_function_SortNumbersAscending")
+{
+	std::vector<double> nonEmptyVector{ 10, 2.4, 7.9, 0, 11 };
+
+	std::vector<double> sortedNonEmptyVector{ 0, 2.4, 7.9, 10, 11 };
+
+	SortNumbersAscending(nonEmptyVector);
+
+	REQUIRE(notEmptyVector == sortedNonEmptyVector);
+}
+
+TEST_CASE("Check_function_MultiplyByLowerValue (lowerValue = 0)")
+{
+	std::vector<double> nonEmptyVector{ 10, 2.4, 7.9, 0, 11 };
+
+	std::vector<double> resultVector{ 0, 0, 0, 0, 0 };
+
+	MultiplyByLowerValue(nonEmptyVector);
+
+	REQUIRE(notEmptyVector == resultVector);
+}
+
+TEST_CASE("Check_function_MultiplyByLowerValue (lowerValue = 1)")
+{
+	std::vector<double> nonEmptyVector{ 3, 4, 1, 2, 6, 5 };
+
+	std::vector<double> resultVector{ 1, 2, 3, 4, 5, 6 };
+
+	MultiplyByLowerValue(nonEmptyVector);
+
+	REQUIRE(notEmptyVector == resultVector);
 }
